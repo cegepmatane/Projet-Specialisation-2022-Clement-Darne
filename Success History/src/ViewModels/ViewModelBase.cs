f6098379c2296e83e6cf1cb7c7d20e227e104c82
Clément Darne <clementdarne8888@gmail.com>
2022-02-04 01:28:08 -0500

    Initialized Avalonia project.

---
+++ Success History/src/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Success_History.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
