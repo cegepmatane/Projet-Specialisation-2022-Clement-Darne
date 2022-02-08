aa69a625ef37dc90722e34a90e15a5a5abe18ad3
Clément Darne <clementdarne8888@gmail.com>
2022-02-08 17:44:59 -0500

    Ajout de la sérialisation de la classe Test. #3

---
+++ Success History/Models/Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Success_History.Models
+{
+    public class Test
+    {
+        public Test(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; set; }
+
+        public void Serialize()
+        {
+            // Directory creation if it does not exist yet
+            if (!Directory.Exists("./data"))
+            {
+                Directory.CreateDirectory("./data");
+            }
+
+            string jsonString = JsonSerializer.Serialize(this);
+            File.WriteAllText("./data/test.json", jsonString);
+        }
+
+        public bool deserialize()
+        {
+            return false;
+        }
+    }
+
+}
--- Success History/ViewModels/MainWindowViewModel.cs
+++ Success History/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,26 @@
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Success_History.Models;
+
 
 namespace Success_History.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        public MainWindowViewModel()
+        {
+            SerializeCommand = ReactiveCommand.Create(() =>
+            {
+                var test = new Test("Hello JSON");
+                test.Serialize();
+            });
+        }
+
         public string Greeting => "Welcome to Avalonia!";
+
+        public ICommand SerializeCommand { get; }
     }
 }
