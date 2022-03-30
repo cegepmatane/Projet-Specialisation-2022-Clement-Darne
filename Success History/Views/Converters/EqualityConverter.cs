c0e7fdaa9742c896829c26ec0727f6d859306f82
Clément Darne <clementdarne8888@gmail.com>
2022-03-30 00:19:09 -0400

    #12 Affichage propre d'un dossier.

---
+++ Success History/Views/Converters/EqualityConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Data.Converters;
+
+namespace Success_History.Views.Converters
+{
+    public class EqualityConverter : IValueConverter
+    {
+        public static bool areEqual(string? str1, string? str2)
+        {
+            if (str1 == null)
+            {
+                if (str2 == null)
+                    return true;
+                else
+                    return false;
+            }
+            if (str2 == null)
+                return false;
+
+            return (str1 == str2);
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string? stringValue = value.ToString();
+            string? stringParameter = parameter as string;
+            
+            return areEqual(stringValue, stringParameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Not supported
+            return null;
+        }
+    }
+}
---
+++ Success History/Views/Converters/InequalityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Data.Converters;
+
+namespace Success_History.Views.Converters
+{
+    public class InequalityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string? stringValue = value.ToString();
+            string? stringParameter = parameter as string;
+            
+            return !EqualityConverter.areEqual(stringValue, stringParameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Not supported
+            return null;
+        }
+    }
+}
